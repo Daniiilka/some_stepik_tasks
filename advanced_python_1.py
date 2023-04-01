55aa3f1545ed4e9240bc6edbf092e87cdd50b2ce
daniil <denrds@gmail.com>
2023-04-01 20:12:00 +0400

    added a few tasks

--- advanced_python_1.py
+++ advanced_python_1.py
@@ -7,6 +7,7 @@ card_number — строка, представляющая собой корре
 и возвращать полученный результат. Если между цифрами в номере имелись
 символы пробела, их следует удалить.
 """
+import collections
 
 
 def hide_card(number):
@@ -234,18 +235,229 @@ numbers — список целых неотрицательных чисел
 numbers. Если список numbers пуст, функция должна вернуть число -1−1."""
 
 
-def get_biggest():
+def get_biggest(numbers: list):
+    # todo
     pass
 
 
-if __name__ == "__main__":
+"""
+Напишите программу, которая вычисляет минимальное расстояние, которое 
+потребуется пройти Тимуру, чтобы посетить оба магазина и вернуться 
+домой. Тимур всегда стартует из дома. Он должен посетить оба магазина, 
+перемещаясь только по имеющимся трём дорожкам, и вернуться назад домой. 
+При этом его совершенно не смутит, если ему придётся посетить один и 
+тот же магазин или пройти по одной и той же дорожке более одного раза.
+Единственная его задача — минимизировать суммарное пройденное расстояние.
+"""
 
-    print(choose_plural(512312, ("цент", "цента", "центов")))
-    print(choose_plural(59, ("помидор", "помидора", "помидоров")))
-    print(choose_plural(23424157, ("огурец", "огурца", "огурцов")))
-    print(choose_plural(240, ("курица", "курицы", "куриц")))
-    print(choose_plural(49324, ("плюмбус", "плюмбуса", "плюмбусов")))
-    print(choose_plural(505, ("утка", "утки", "уток")))
-    print(choose_plural(666, ("шкаф", "шкафа", "шкафов")))
-    print(choose_plural(11, ("стул", "стула", "стульев")))
-    print(choose_plural(3458438435812, ("доллар", "доллара", "долларов")))
+
+def get_min_path():
+    d1 = int(input())
+    d2 = int(input())
+    d3 = int(input())
+    path1 = d1 + d2 + d3
+    path2 = d1 + d1 + d2 + d2
+    path3 = d1 + d3 + d3 + d1
+    path4 = d2 + d3 + d3 + d2
+    print(min(path2, path1, path4, path3))
+
+
+"""
+В русском и английском языках есть буквы, которые выглядят одинаково. 
+Вот список английских букв "AaBCcEeHKMOoPpTXxy", а вот их русские 
+аналоги "АаВСсЕеНКМОоРрТХху". Напишите программу, которая для трёх 
+букв из данных списков букв определяет, русские они, английские или 
+и те и другие (смесь русских и английских букв)."""
+
+
+def similar_letters():
+    en, ru = "AaBCcEeHKMOoPpTXxy", "АаВСсЕеНКМОоРрТХху"
+    letters = [str(input()) for _ in range(3)]
+    if letters[0] in en and letters[1] in en and letters[2] in en:
+        print("en")
+    elif letters[0] in ru and letters[1] in ru and letters[2] in ru:
+        print("ru")
+    else:
+        print("mix")
+
+
+"""
+Дана последовательность натуральных чисел от 11 до nn. Напишите 
+программу, которая сначала располагает в обратном порядке часть 
+элементов этой последовательности от элемента с номером XX до 
+элемента с номером YY, а затем от элемента с номером AA до элемента 
+с номером BB.
+"""
+
+
+def reverse_nums():
+    # 9 2 5 6 9
+
+    n, x, y, a, b = [int(el) for el in input().split()]
+    sequence = list(range(1, n + 1))
+    first_reverse = list(reversed(sequence[x - 1: y]))
+    second_reverse = list(reversed(sequence[a - 1: b]))
+
+    print(*(sequence[: x - 1] + first_reverse + second_reverse + sequence[b:]))
+
+
+"""Более одного
+Дана последовательность неотрицательных целых чисел. Напишите программу, которая выводит те числа, которые встречаются 
+в данной последовательности более одного раза.
+
+Формат входных данных
+На вход программе подается строка, содержащая целые неотрицательные числа, разделенные пробелом.
+
+Формат выходных данных
+Программа должна вывести те числа, которые встречаются в данной строке более одного раза, разделяя их пробелом. 
+Числа должны быть расположены в порядке возрастания и не должны повторяться."""
+
+
+def counter():
+    nums = list(int(el) for el in input().split())
+    uniq_nums = set(el for el in nums if nums.count(el) > 1)
+    return list(uniq_nums)
+
+
+"""Максимальная группа
+Назовем набор различных натуральных чисел группой. Например: (13, 4, 22, 40)(13,4,22,40). Тогда длиной группы будем 
+считать количество чисел в группе. Например, длина группы (13, 4, 22, 40)(13,4,22,40) равна 44.
+
+Дана последовательность натуральных чисел от 11 до nn включительно. Напишите программу, которая группирует все числа 
+данной последовательности по сумме их цифр и определяет длину группы, содержащей наибольшее количество чисел."""
+
+
+def max_group(n):
+    nums = list(range(1, n + 1))
+    result_groups = []
+    for index in range(1, len(nums) + 1):
+        result_groups.append([el for el in nums if sum(map(int, list(str(el)))) == index])
+    return len(sorted(result_groups, key=len)[-1])
+
+
+"""Трудности перевода
+Зачастую переводить сериалы, не теряя изначальный смысл, невозможно, особенно за счет игр слов. Сумасшедший режиссер 
+хочет снять сериал, в котором бы в целях эксперимента задействовал как можно больше языков, чтобы пользоваться красотой 
+каждого из них. Тем не менее если задействовать слишком много языков, то сериал станет непонятен абсолютно всем, поэтому 
+режиссер достает случайных людей на улице и спрашивает их, какие языки они знают, таким образом он будет использовать 
+языки которые знают все из них.
+
+Напишите программу, которая определяет, какие языки будут использоваться в сериале.
+
+"""
+
+
+def translation_issues():
+    n = int(input())
+    user_languages = []
+    for _ in range(n):
+        user_languages.append(list(el.strip(' ') for el in input().split(',')))
+    result = sorted(set.intersection(*map(set, user_languages)))
+    print(', '.join(result) if len(result) > 0 else 'Сериал снять не удастся')
+
+
+"""Напишите программу, которая находит все схожие слова для заданного слова. Слова называются схожими, если имеют 
+одинаковое количество и расположение гласных букв. При этом сами гласные могут различаться."""
+
+
+def transform(word):
+    vowels = 'ауоыиэяюёе'
+    transformed = ''
+    for el in word:
+        if el in vowels:
+            transformed += '1'
+        else:
+            transformed += '0'
+    return transformed
+
+
+def similar_words():
+    init_word = str(input())
+    transformed_init = transform(init_word)
+    transformed_init = transformed_init[:transformed_init.rfind('1')]
+
+    count = int(input())
+    words = [str(input()) for _ in range(count)]
+    for word in words:
+        transformed_word = transform(word)
+        if transformed_word[:transformed_word.rfind('1')] == transformed_init:
+            print(word)
+
+
+"""В онлайн-школе "BEEGEEK" сотрудникам положена корпоративная почта, которая формируется как <имя-фамилия>@beegeek.bzz, 
+например, timyr-guev@beegeek.bzz. При таком подходе существует проблема тёзок. Для решения такой проблемы было решено 
+приписывать справа номер.
+
+Тогда первый Тимур Гуев получает ящик timyr-guev@beegeek.bzz (без номера), второй — timyr-guev1@beegeek.bzz, третий — 
+timyr-guev2@beegeek.bzz, и так далее.
+
+Вам дан список уже занятых ящиков в порядке их выдачи и имена-фамилии новых сотрудников в заранее подготовленном виде 
+(латиницей с символом - между ними). Напишите программу, которая раздает корпоративные ящики новым сотрудникам школы."""
+
+
+def corporate_mail():
+    mail_service = '@beegeek.bzz'
+    n = int(input())
+    mail_list = [str(input()).replace(mail_service, '') for _ in range(n)]
+    find_n = int(input())
+    find_mail = [str(input()) for _ in range(find_n)]
+
+
+"""Вам доступен текстовый файл files.txt, содержащий информацию о файлах. Каждая строка файла содержит три значения, 
+разделенные символом пробела — имя файла, его размер (целое число) и единицы измерения:
+
+cant-help-myself.mp3 7 MB
+keep-yourself-alive.mp3 6 MB
+bones.mp3 5 MB
+...
+Напишите программу, которая группирует данные файлы по расширению, определяя общий объем файлов каждой группы, и выводит 
+полученные группы файлов, указывая для каждой ее общий объем. Группы должны быть расположены в лексикографическом 
+порядке названий расширений, файлы в группах — в лексикографическом порядке их имен."""
+
+
+def sum_size(size: list):
+    from decimal import Decimal
+    size_file = {'B': 1, 'KB': 1024, 'MB': 1024 ** 2, 'GB': 1024 ** 3}
+    size_in_bytes = []
+    for el in size:
+        size_in_bytes.append(Decimal(int(size_file[el[1]]) * int(el[0])), )
+    sum_size_bytes = sum(size_in_bytes)
+    if sum_size_bytes > 1023:
+        size_in_kb = round(sum_size_bytes/1024)
+        actual_size = f'{size_in_kb} KB'
+        if size_in_kb > 1023:
+            size_in_mb = round(size_in_kb/1024)
+            actual_size = f'{size_in_mb} MB'
+            if size_in_mb > 1023:
+                size_in_gb = round(size_in_mb/1024)
+                actual_size = f'{size_in_gb} GB'
+    return actual_size
+
+
+def sort_files():
+
+    with open('files.txt', 'r') as data:
+        files_list = data.readlines()
+    files_dict = dict()
+    for raw_file in files_list:
+        raw_file_info = raw_file.split()
+        file_name, file_type = raw_file_info[0].split('.')
+        size = raw_file_info[1:]
+
+        if file_type not in files_dict:
+            files_dict[file_type] = {}
+            files_dict[file_type]['names'] = file_name
+            files_dict[file_type]['size'] = [size]
+        else:
+            files_dict[file_type]['names'] += f' {file_name}'
+            files_dict[file_type]['size'].append(size)
+    for file_type in sorted(files_dict):
+        [print(f'{name}.{file_type}') for name in sorted(files_dict[file_type]['names'].split())]
+        print('----------')
+        sum_suze = sum_size(files_dict[file_type]["size"])
+        print(f'Summary: {sum_suze}')
+        print()
+
+
+if __name__ == "__main__":
+    sort_files()
